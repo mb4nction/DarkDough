3e60a93bfb9875da6156cdcd6ee9c7bf19bf1024
Eugene Kurennyy <ek@anahoret.com>
2012-01-23 10:02:39 +0200

    Base Static controller was generated

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,7 @@
+class StaticController < ApplicationController
+  def home; end
+  def about; end
+  def how_it_works; end
+  def faq; end
+  def terms_of_use; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Pfm::Application.routes.draw do
+
+  match '/home' => 'static#home'
+  match '/about' => 'static#about'
+  match '/how_it_works' => 'static#how_it_works'
+  match '/faq' => 'static#faq'
+  match '/terms_of_use' => 'static#terms_of_use'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +56,7 @@ Pfm::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static#home'
 
   # See how all your routes lay out with "rake routes"
 
