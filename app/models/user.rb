06674abf0f119f25abc9b8c0425a45a3dc275073
Eugene Kurennyy <ek@anahoret.com>
2012-01-23 17:48:58 +0200

    User model implementation was started

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  attr_accessible :first_name, :last_name, :phone_number, :email,
+                  :password, :password_confirmation, :country, :tos_confirmation
+  attr_accessor   :tos_confirmation
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +10,9 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  validates :first_name, :last_name, :country, :presence => true
+  validates :first_name, :last_name, :length => { :minimum => 0, :maximum => 128 }
+
+  validates :tos_confirmation, :acceptance => true, :if => :new_record?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120123135957) do
+ActiveRecord::Schema.define(:version => 20120123150300) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -25,9 +25,15 @@ ActiveRecord::Schema.define(:version => 20120123135957) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone_number"
+    t.string   "country"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["first_name"], :name => "index_users_on_first_name"
+  add_index "users", ["last_name"], :name => "index_users_on_last_name"
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
