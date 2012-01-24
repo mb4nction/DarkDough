4c09b6ea908c5cf823a02f8889e1330976257377
Eugene Kurennyy <ek@anahoret.com>
2012-01-24 09:45:28 +0200

    Tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,20 @@
 class User < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :phone_number, :email,
-                  :password, :password_confirmation, :country, :tos_confirmation
   attr_accessor   :tos_confirmation
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :first_name, :last_name, :phone_number, :email,
+                  :password, :password_confirmation, :country, :tos_confirmation,
+                  :email, :password, :password_confirmation, :remember_me
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-
   validates :first_name, :last_name, :country, :presence => true
-  validates :first_name, :last_name, :length => { :minimum => 0, :maximum => 128 }
+  validates :first_name, :last_name, :length => { :maximum => 30 }
+  validates :password, :presence => true, :confirmation => true, :length => { :within => 6..40 }
 
   validates :tos_confirmation, :acceptance => true, :if => :new_record?
+  validates :email, :uniqueness => true
 end
