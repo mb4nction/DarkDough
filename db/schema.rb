7e0252032a5177daeb24f3db74fad473c9ff0946
Eugene Kurennyy <ek@anahoret.com>
2012-01-25 11:21:08 +0200

    ContactUs model was added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120123150300) do
+ActiveRecord::Schema.define(:version => 20120125063920) do
+
+  create_table "contact_us", :force => true do |t|
+    t.string   "full_name"
+    t.string   "email"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
