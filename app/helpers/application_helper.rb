639fdb4fd68f721bc9b572c248c939ad8c6d88a2
Eugene Kurennyy <ek@anahoret.com>
2012-02-01 11:35:44 +0200

    Acumen test menu helper added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
     site_title = "pfm"
     @title.nil? ? "#{site_title}" : "#{@title}"
   end
+
+  def acumen_test_menu(user)
+    if !user.acumen_tests.any?
+      link_to "Create Acumen Test", new_acumen_test_path
+    elsif !user.acumen_tests.last.finished == true
+      link_to "Continue Acumen Test", edit_acumen_test_path(user.acumen_tests.first)
+    elsif user.acumen_tests.last.finished?
+      link_to "Acumen Test Result", acumen_test_path(user.acumen_tests.first)
+    end
+  end
 end
