94348974c1fa1b07ce6fbd60c083f0d050a14c22
Eugene Kurennyy <ek@anahoret.com>
2012-02-10 09:48:41 +0200

    Tabs added to profile page

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,4 +2,19 @@ $(function(){
   $('.user').click(function(){
     $('.user-menu').toggle();
   })
+
+  // tabs on user profile page
+  $(".tabs .block:not('.active-tab')").hide();
+
+  $(".profile-nav li").click(function(){
+    thisId = $(this).attr("class").split(' ')[0];
+
+    if (!$(this).hasClass('active-tab')) {
+      $(".profile-nav li").removeClass('active-tab');
+      $(".tabs .block").removeClass('active-tab');
+      $(this).toggleClass('active-tab');
+      $(".tabs").find('#' + thisId).addClass('active-tab');
+    }
+  });
 });
+
