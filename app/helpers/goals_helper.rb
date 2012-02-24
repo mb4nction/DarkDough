b32185132050ea256cd54d12b82ca13f37da6a76
Eugene Kurennyy <ek@anahoret.com>
2012-02-24 15:05:21 +0200

    minor fix

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -4,7 +4,7 @@ module GoalsHelper
     current_user.goals.each do |goal|
       user_accounts_ids << goal.account_ids
     end
-    user_accounts_ids = user_accounts_ids.uniq!.flatten!
+    user_accounts_ids = user_accounts_ids.uniq.flatten
     user_accounts_ids.include?(account.id) ? true : false
   end
 
