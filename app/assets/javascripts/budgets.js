8d8c95102996c8aa1ff5b3fbb9079b32d2a07d79
Eugene Kurennyy <ek@anahoret.com>
2012-03-21 15:38:59 +0200

    User can create a new budget via ajax

--- app/assets/javascripts/budgets.js
+++ app/assets/javascripts/budgets.js
@@ -222,6 +222,38 @@ $(function() {
     })
   };
 
+  // forms. jQuery.ui used
+  $('#create_new_budget').dialog({
+    autoOpen: false,
+    resizable: false,
+    modal: true,
+    minWidth: 490,
+    dialogClass: 'my_dialog',
+    title: "New budget",
+    buttons: {
+      'Save': function() {
+        $('#new_budget').submit()
+        $(this).dialog('close')
+      },
+      'Cancel': function() {
+        $(this).dialog('close')
+      }
+    }
+  })
+
+  $('.new-budget').on("click", function() {
+    $('#create_new_budget').dialog("open");
+    return false;
+  });
+
+  $('#budget_category').chosen();
+  $('.start #budget_start_1i').chosen();
+  $('.start #budget_start_2i').chosen();
+  $('.period #period').chosen();
+
+  $('.radiogroup').customizeRadiobuttons();
+
+
   timelinePeriodLinks();
 
   budgetOptions();
