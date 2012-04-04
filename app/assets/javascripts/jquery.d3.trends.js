e9ec5f169b873d736aa60d188389d39d576bb2bb
Eugene Kurennyy <ek@anahoret.com>
2012-04-04 17:07:18 +0300

    Trends page: spending and income

--- app/assets/javascripts/jquery.d3.trends.js
+++ app/assets/javascripts/jquery.d3.trends.js
@@ -10,43 +10,60 @@
       var $containersForDrawing = $('div[class*=container].svg');
 
       $containersForDrawing.each(function() {
-        // get data
-        var data = $(this).find('.data').text().split(','),
-            elemCount = data.length;
-            sum = parseFloat($(this).find('.data-sum').text());
-        for (i in data) {
-          data[i] = Math.round((parseFloat(data[i]) / sum) * 1000) / 10;
-        };
-
-        if (!isNaN(sum)) {
           var containerClass = $(this).attr('class').split(/\s+/)[0];
           var partialId = $(this).parent().attr('id');
-          var chart = d3.select("#" + partialId + " ." + containerClass)
-                        .append("svg")
-                        .attr("class", "chart")
-                        .attr("width", 500)
-                        .attr("height", 200);
-          var x = d3.scale.linear()
-                    .domain([0, d3.max(data)])
-                    .range([40, 500]);
 
-          chart.selectAll("rect").data(data).enter()
-                                 .append("rect")
-                                 .attr("y", function(d, i) { return i * 25; })
-                                 .attr("width", x)
-                                 .attr("height", 25);
+        var r = 600,
+            format = d3.format(",d"),
+            fill = d3.scale.category10();
 
-          var y = d3.scale.ordinal().domain(data).rangeBands([0, elemCount * 25]);
+        // return a new layout pack with size equals to SVG size (width & height)
+        var bubble = d3.layout.pack()
+            .sort(null)
+            .size([r, r]);
 
-          chart.selectAll("text").data(data).enter()
-                                 .append("text")
-                                 .attr("x", x)
-                                 .attr("y", function(d) {return y(d) + y.rangeBand() / 2})
-                                 .attr("dx", -3)
-                                 .attr("dy", ".35em")
-                                 .attr("text-anchor", "end")
-                                 .text(String);
+        // return SVG container => svg.bubble
+        var vis = d3.select("#" + partialId + " ." + containerClass).append("svg")
+            .attr("width", r)
+            .attr("height", r)
+            .attr("class", "bubble");
+
+        // creating a nodes
+        var json = $.parseJSON($(this).find('.json').text());
+
+        var node = vis.selectAll("g.node")
+            .data(bubble.nodes(classes(json))
+            .filter(function(d) { return !d.children; }))
+          .enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+        node.append("title")
+            .text(function(d) { return d.className + ": " + format(d.value); });
+
+        node.append("circle")
+            .attr("r", function(d) { return d.r; })
+            .style("fill", function(d) { return fill(d.packageName); });
+
+        node.append("text")
+            .attr("text-anchor", "middle")
+            .attr("dy", ".3em")
+            .attr("font-size", function(d) { return Math.sqrt(d.r * .03) + "em"; })
+            .text(function(d) { return d.className.substring(0, d.r / 3); });
+
+        // Returns a flattened hierarchy containing all leaf nodes under the root.
+        function classes(root) {
+          var classes = [];
+
+          function recurse(name, node) {
+            if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
+            else classes.push({packageName: name, className: node.name, value: node.size});
+          }
+
+          recurse(null, root);
+          return {children: classes};
         }
+
       });
     };
 
--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,15 +1,39 @@
 class TrendsController < ApplicationController
   before_filter :authenticate_user!
+  require 'active_support/core_ext/date/conversions'
 
   def index
     @top_merchant = current_user.top_merchant
     @most_purchased  = current_user.most_purchased
     @most_expensive = current_user.transactions.find(:all, :order => 'amount DESC').first
-    
-    @spending_transactions = current_user.transactions.spending_transactions.
-                                          search(params[:trends_start], params[:trends_end], 'spending')
-    @income_transactions = current_user.transactions.by_category('Income').
-                                        search(params[:trends_start], params[:trends_end], 'income')
+
+    @spending_transactions = current_user.transactions.spending_transactions.search(params[:trends_start], params[:trends_end], 'spending')
+    @income_transactions = current_user.transactions.by_category('Income').search(params[:trends_start], params[:trends_end], 'income')
+
+    @spending_by_category_json = {
+      :name => 'flare', :children => category_listed(@spending_transactions).map{ |category, amount| {
+        :name => category, :children => [{ :name => category, :size => amount }] }}}
+
+
+    @spending_over_time_json = {
+      :name => 'flare', :children => time_listed(@spending_transactions).map{ |month, amount| {
+        :name => Date::MONTHNAMES[month.to_i], :children => [{ :name => Date::MONTHNAMES[month.to_i], :size => amount }] }}}
+
+    @spending_by_merchant_json = {
+      :name => 'flare', :children => merchant_listened(@spending_transactions).map{ |description, amount| {
+        :name => description, :children => [{ :name => description, :size => amount }] }}}
+
+    @income_by_category_json = {
+      :name => "flare", :children => category_listed(@income_transactions).map { |category, amount| {
+          :name => category, :children => [{ :name => category, :size => amount.to_f}] }}}
+
+    @income_over_time_json = {
+      :name => 'flare', :children => time_listed(@income_transactions).map{ |month, amount| {
+        :name => Date::MONTHNAMES[month.to_i], :children => [{ :name => Date::MONTHNAMES[month.to_i], :size => amount }] }}}
+
+    @income_by_merchant_json = {
+      :name => 'flare', :children => merchant_listened(@income_transactions).map{ |description, amount| {
+        :name => description, :children => [{ :name => description, :size => amount }] }}}
 
     @income_amount = current_user.transactions.by_category('Income').find_amount(params[:trends_start], params[:trends_end])
     @spending_amount = current_user.transactions.spending_transactions.find_amount(params[:trends_start], params[:trends_end])
@@ -17,5 +41,52 @@ class TrendsController < ApplicationController
       format.html
       format.js
     end
+  end
+
+  private
+
+  def category_listed(transactions)
+    hash_array = []
+    categories = transactions.map{ |transaction| transaction.category }.uniq
+    categories.each do |category|
+      amounts = []
+      transactions.each do |transaction|
+        amounts << transaction.amount if transaction.category == category
+      end
+      hash_array << amounts.sum
+      hash_array << category
+    end
+    hash_array = hash_array.reverse
+    Hash[*hash_array]
+  end
+
+  def time_listed(transactions)
+    hash_array = []
+    months = transactions.map{ |transaction| transaction.created_at.month }.uniq
+    months.each do |month|
+      amounts = []
+      transactions.each do |transaction|
+        amounts << transaction.amount.to_i if transaction.created_at.month == month
+      end
+      hash_array << amounts.sum
+      hash_array << month.to_s
+    end
+    hash_array = hash_array.reverse
+    Hash[*hash_array]
+  end
+
+  def merchant_listened(transactions)
+    hash_array = []
+    descriptions = transactions.map{ |transaction| transaction.description }.uniq
+    descriptions.each do |description|
+      amounts = []
+      transactions.each do |transaction|
+        amounts << transaction.amount.to_i if transaction.description == description
+      end
+      hash_array << amounts.sum
+      hash_array << description
+    end
+    hash_array = hash_array.reverse
+    Hash[*hash_array]
   end
 end
