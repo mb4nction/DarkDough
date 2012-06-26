cc7c1165e8b24ccb9f56fd7a5637e60b9aae30d5
Eugene Kurennyy <ek@anahoret.com>
2012-06-26 11:58:24 +0300

    deploy.rb updated to nginx using

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,10 +24,6 @@ role :web, "#{deploy_by_user}@#{ip}"
 role :app, "#{deploy_by_user}@#{ip}"
 role :db,  "#{deploy_by_user}@#{ip}", :primary => true # This is where Rails migrations will run
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
   desc "Insert test data"
   task :insert_test_data, :roles => :app  do
@@ -39,23 +35,4 @@ namespace :deploy do
   end
 end
 
-namespace :rvm do
-  set :passenger_standalone_port, 3035
-
-  desc "trust .rvmrc in new release directory"
-  task :trust_rvmrc do
-    run "rvm rvmrc trust #{current_release}"
-  end
-
-  desc "(Re)start Phusion Passenger Standalone (is used by Apache as a reverse proxy)."
-  task :restart_passenger_standalone do
-    run "if [[ -s #{current_release}/tmp/pids/passenger.#{passenger_standalone_port}.pid ]]; then cd #{current_release} && passenger stop -p #{passenger_standalone_port}; fi"
-    run "cd #{current_release} && passenger start -a 127.0.0.1 -p #{passenger_standalone_port} -d -e #{rails_env}"
-  end
-end
-
-# after 'deploy:symlink', 'deploy:insert_test_data'
-
-after "deploy:create_symlink", "rvm:trust_rvmrc"
-after 'deploy:create_symlink', 'rvm:restart_passenger_standalone'
-
+after 'deploy:symlink', 'deploy:insert_test_data'
