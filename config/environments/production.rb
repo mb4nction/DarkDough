cdf2d96dcf76f18700681f1b5957b1c93b1bb02d
Eugene Kurennyy <eugene.kurennyy@gmail.com>
2012-07-11 08:54:23 +0300

    default action mailer host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ Pfm::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => 'pfm.anahoret.com' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
@@ -70,5 +72,4 @@ Pfm::Application.configure do
     :password             => "#{PASSWORD}",
     :authentication       => 'plain',
     :enable_starttls_auto => true  }
-
 end
