75bf967ecd6c1a977f0771b00f31b5fafb882cdc
Ildar Manzhikov <manzhikov@gmail.com>
2012-10-18 20:38:21 +0800

    Added db:migrate to deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,7 @@ default_run_options[:pty] = true
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
+after 'deploy', 'deploy:migrate'
 
 namespace :deploy do
   task :start do ; end
